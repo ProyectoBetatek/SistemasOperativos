813d4729b99801ad9fdd5a5d8cfaa18585016501
ProyectoBetatek <69726228+ProyectoBetatek@users.noreply.github.com>
2020-11-04 21:01:23 -0300

    Agregados log al menu

--- Scripts-Betatek/ABML/bin/abml_menu.sh
+++ Scripts-Betatek/ABML/bin/abml_menu.sh
@@ -13,10 +13,12 @@ do
 	echo -e "\n\t\t\t---MENÚ PRINCIPAL--- \n
 
 \t--USUARIOS--\t\t|\t--GRUPOS-- \n
-\t1- Crear Usuario\t|\t5- Crear Grupo
-\t2- Modificar Usuario\t|\t6- Modificar Grupo 
-\t3- Eliminar Usuario\t|\t7- Eliminar Grupo 
-\t4- Mostrar Usuarios\t|\t8- Mostrar Grupos 
+\t1- Crear Usuario\t|\t4- Crear Grupo 
+\t2- Eliminar Usuario\t|\t5- Eliminar Grupo 
+\t3- Mostrar Usuarios\t|\t6- Mostrar Grupos
+\t------------------\tLogs\t-----------------
+\t7- Intentos de login fallidos.
+\t8- Login exitoso.
 \t-------------------\t|\t-------------------
 
 \t\t\t\t0-Salir\n"
@@ -28,38 +30,43 @@ do
 	case $op in
 		1) 
 			sh ./alta_usuario.sh
+	
 		;;
 
 		2) 
-			sh ./modificar_usuario.sh
+			 sh ./eliminar_usuario.sh
 		;;
 
 
 		3) 
-			sh ./eliminar_usuario.sh
+			sh ./listar_usuario.sh
+
 		;;
 
 
-		4) 
-			sh ./listar_usuario.sh
+		4)	
+			sh ./alta_grupo.sh
+
 		;;
 
 
 		5) 
-			sh ./alta_grupo.sh
-		;;
-
-		6) 
-			sh ./modificar_grupo.sh
-		;;
-
-		7) 
 			sh ./eliminar_grupo.sh
 		;;
 
-		8) 
+		6) 
 			sh ./listar_grupo.sh
+
 		;;
+
+		7) 
+			sh ./Log_Fallido.sh	
+		;;
+
+		8) 
+			sh ./Log_Exitoso.sh	
+		;;
+
 
 		0) clear 
 		echo -e "\n\e[0;32m\n\t\t---FIN DE EJECUCION---\e[0m\n"
@@ -76,3 +83,4 @@ do
 done
 
 #FIN MENU_ABML
+
